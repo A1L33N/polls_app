9bb00efbe6658d4b4b48958128a7cf021acf4eb0
Aileen Cacayorin <acacayorin@gmail.com>
2016-01-06 16:16:02 -0800

    add question details views, namespacing, urls

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 
 from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
     url(r'^$', views.home_page),
+    url(r'^admin/', admin.site.urls),
+
 ]
