b21deec8990304f6151cc6b16dbe34c30d23538a
Aileen Cacayorin <acacayorin@gmail.com>
2016-01-10 11:32:00 -0800

    customize django admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice #indicates Choice objects are edited on Question admin page
+    extra = 4 #provides 4 fields as default
 
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
@@ -12,4 +16,11 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+
+    inlines = [ChoiceInline] #indicates Choice objects are edited on Question admin page
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 admin.site.register(Question, QuestionAdmin)
